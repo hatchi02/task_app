c0ebbfafade1a4f8f8aa466ed4c7dd8a7460853d
hatchi02 <wataru.hachi8@gmail.com>
2022-08-22 16:37:50 +0900

    first branch uplode

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "posts/index" => "posts#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
